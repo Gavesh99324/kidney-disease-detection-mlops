c26d145c71def9954a06ed4000846f5e45800cf8
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-11-03 23:42:21 +0530

    Added model training stage and updated configuration files

---
+++ src/cnnClassifier/pipeline/stage_03_model_training.py
@@ -0,0 +1,7 @@
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.model_training import Training
+from cnnClassifier import logger
+
+
+STAGE_NAME = "Training"
+
