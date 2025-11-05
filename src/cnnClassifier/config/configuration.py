c1f64988d45deab98a4d5bb3b93ef1a22b7875fa
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-11-06 05:21:15 +0530

    Update model evaluation stage, configurations, and notebooks

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,10 +1,9 @@
 from cnnClassifier.constants import *
-from cnnClassifier.utils.common import read_yaml, create_directories
+from cnnClassifier.utils.common import read_yaml, create_directories, save_json
 from cnnClassifier.entity.config_entity import (
-    DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig)
+    DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig, EvaluationConfig)
 import os
 from pathlib import Path
-
 
 
 class ConfigurationManager:
@@ -73,3 +72,15 @@ class ConfigurationManager:
         )
 
         return training_config
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-images",
+            mlflow_uri=self.params,
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+
